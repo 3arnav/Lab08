4495f8a9c435153da7775386c5713ea6e9130c4b
Arnav Chandra <chandra3@ualberta.ca>
2026-03-05 17:40:16 -0700

    failed hasCity test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -8,6 +8,7 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
+
         assertTrue(list.hasCity(calgary));
     }
 }
